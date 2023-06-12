6df4dc56b3d466fcc54c9cff2f94bce2e0253883
Martin <cheyuan0621@icloud.com>
2023-06-12 16:34:35 +0800

    0612_乙級第一題_p25

---
+++ back/ad.php
@@ -0,0 +1,46 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+    <p class="t cent botli">動態文字管理</p>
+    <form method="post" action="?do=tii">
+        <table width="100%">
+            <tbody>
+                <tr class="yel">
+                    <td width="68%">替代文字</td>
+                    <td width="7%">顯示</td>
+                    <td width="7%">刪除</td>
+                    <td></td>
+                </tr>
+            <?php
+
+                $rows=$Ad->all();
+                foreach($rows as $row){
+            ?>
+                <tr>
+                    <td>
+                        <input type="text" name="text" value="<?=$row['text'];?>" style='width:95%'>
+                    </td>
+                    <td>
+                        <input type="checkbox" name="sh" value="<?=$row['id'];?>">
+                    </td>
+                    <td>
+                        <input type="checkbox" name="del" value="<?=$row['id'];?>">
+                    </td>
+                    <td>
+                        
+                    </td>
+                </tr>
+            <?php
+            }
+            ?>                
+            </tbody>
+        </table>
+        <table style="margin-top:40px; width:70%;">
+            <tbody>
+                <tr>
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/ad.php')" value="新增動態文字廣告"></td>
+                    <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
+                </tr>
+            </tbody>
+        </table>
+
+    </form>
+</div>
